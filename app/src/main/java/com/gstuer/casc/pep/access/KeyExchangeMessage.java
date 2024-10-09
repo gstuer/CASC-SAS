63553e64bb662ed0a10ef0634995279e092f94b7
Moritz Gstuer <moritz@gstuer.com>
2024-10-09 17:21:59 +0200

    Add key exchange request & response message class

---
+++ app/src/main/java/com/gstuer/casc/pep/access/KeyExchangeMessage.java
@@ -0,0 +1,27 @@
+package com.gstuer.casc.pep.access;
+
+import com.gstuer.casc.pep.access.cryptography.DigitalSignature;
+import com.gstuer.casc.pep.access.cryptography.EncodedKey;
+import com.gstuer.casc.pep.serialization.JsonProcessor;
+import com.gstuer.casc.pep.serialization.SerializationException;
+
+import java.io.Serial;
+import java.net.InetAddress;
+
+public class KeyExchangeMessage extends AccessControlMessage<EncodedKey> {
+    @Serial
+    private static final long serialVersionUID = -1138137919785740628L;
+
+    public KeyExchangeMessage(InetAddress destination, DigitalSignature signature, EncodedKey encodedKey) {
+        super(destination, signature, encodedKey);
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return new JsonProcessor().convertToJson(this);
+        } catch (SerializationException exception) {
+            throw new IllegalStateException(exception);
+        }
+    }
+}
---
+++ app/src/main/java/com/gstuer/casc/pep/access/KeyExchangeRequestMessage.java
@@ -0,0 +1,26 @@
+package com.gstuer.casc.pep.access;
+
+import com.gstuer.casc.pep.access.cryptography.DigitalSignature;
+import com.gstuer.casc.pep.serialization.JsonProcessor;
+import com.gstuer.casc.pep.serialization.SerializationException;
+
+import java.io.Serial;
+import java.net.InetAddress;
+
+public class KeyExchangeRequestMessage extends AccessControlMessage<String> {
+    @Serial
+    private static final long serialVersionUID = -6381850339226959873L;
+
+    public KeyExchangeRequestMessage(InetAddress destination, DigitalSignature signature, String algorithmIdentifier) {
+        super(destination, signature, algorithmIdentifier);
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return new JsonProcessor().convertToJson(this);
+        } catch (SerializationException exception) {
+            throw new IllegalStateException(exception);
+        }
+    }
+}
